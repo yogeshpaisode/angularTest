a8c677a3e0c4ee9a5ea90faa9b3077de0aaf2d7b
Yogesh D.Paisode <yogesh.paisode@gmail.com>
2018-12-21 16:35:15 +0530

    files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import HospitalComponent from './components/hostpital/hospitalComponent';
+import DepartmentComponent from './components/department/department.component';
+import CommonComponent from './components/common/common.component';
+
+const appRoutes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'hospital' },
+  { path: 'hospital', component: HospitalComponent },
+  { path: 'department', component: DepartmentComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HospitalComponent,
+    DepartmentComponent,
+    CommonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/hostpital/hospitalComponent.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'hostpital',
+  templateUrl: './hostpital.component.html'
+})
+export default class HospitalComponent {
+
+}
